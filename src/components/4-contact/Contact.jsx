9f2cb1da0015c3bce8a7f02feae090364fa60b1f
Mostafa Hendawy <mostafahendawy26207@gmail.com>
2025-08-02 02:00:53 +0300

    contact.js without animation

--- src/components/4-contact/Contact.jsx
+++ src/components/4-contact/Contact.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import './contact.css'
 
 function Contact() {
   return (
-    <div>
-      Contact
-    </div>
+   <section className='cotact-us'>
+      <h1 className='title'>
+        <span className='icon-mail-envelope-closed'></span>
+        Contact us
+      </h1>
+      <p className='sub-title'>contact us fo more information and get notified
+        when I publish something new
+      </p>
+   <div className="flex">
+       <form>
+       <div className='flex' style={{marginBottom: '2rem'}}>
+          <label htmlFor="email">Email Address</label>
+          <input type="email" name="" id="email" />
+       </div>
+
+        <div className='flex'>
+            <label htmlFor="message">Your Message</label>
+            <textarea name="" id="message"></textarea>   
+          </div>  
+        <button className='submit'>Submit</button>
+      </form>
+      <div className="animation"></div>
+   </div>
+   </section>
   )
 }
 
 export default Contact
+
