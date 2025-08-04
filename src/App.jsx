f0d55ddb392247b33ae6b59497843fbc51f79f16
Mostafa Hendawy <mostafahendawy26207@gmail.com>
2025-08-05 01:51:41 +0300

    show and hide the arrow

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,21 @@ import Hero from "./components/2-hero/Hero"
 import Main from "./components/3-main/Main"
 import Contact from "./components/4-contact/Contact"
 import Footer from "./components/5-footer/Footer"
+import { useEffect, useState } from 'react'
 
 
 function App() {
+const [showArrow, setShowArrow] = useState(false);
 
-
+  useEffect(() => {
+   window.addEventListener('scroll', () => {
+     if (scrollY < 300) {
+      setShowArrow(false)
+    } else {
+      setShowArrow(true);
+    }
+   })
+  }, [])
   return (
     <div id='up' className="container">
       <Header />
@@ -23,9 +33,11 @@ function App() {
             <div className="divider" />
 
       <Footer />
-     <a href="#up">
+
+    <a style={{opacity: showArrow ? 1 : 0, transition: '1s'}} href="#up">
        <button className='icon-circle-up'></button>
      </a>
+  
     </div>
   )
 }
