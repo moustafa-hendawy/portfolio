0056d19759edbf8308d02032c99fb30ce7509b78
Mostafa Hendawy <mostafahendawy26207@gmail.com>
2025-08-05 16:39:14 +0300

    dark and light mode

--- src/components/1-header/Header.jsx
+++ src/components/1-header/Header.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './header.css'
 import { useState } from 'react'
 function Header() {
     const [showMenu, setShowMenu] = useState(false);
+    const [theme, setTheme] = useState(localStorage.getItem('currentMode') ?? 'dark');
+
+    useEffect(() => {
+      if (theme === 'dark') {
+        document.body.classList.remove('light');
+        document.body.classList.add('dark');
+      } else {
+        document.body.classList.remove('dark');
+        document.body.classList.add('light');
+      }
+    },[theme])
+
+
   return (
     <header className='flex'>
         <button className='menu icon-menu' onClick={() => setShowMenu(true)}></button>
@@ -26,10 +39,13 @@ function Header() {
         </li>
        </ul>
       </nav>
-      <button className='moon-mode'>
-        <span className='icon-IcoMoon'></span>
-        {/* <span className="icon-nightlight_round"></span> */}
-        {/* <img src="/nightlight_round.svg" alt="dfdf" /> */}
+      <button className='moon-mode' onClick={() => {
+        //add to LS
+        localStorage.setItem('currentMode', theme === 'dark' ? 'light' : 'dark');
+        //Get value from LS
+        setTheme(localStorage.getItem('currentMode'));
+      }}>
+        {theme === 'dark' ? (<span className='icon-IcoMoon'></span>) : (<span className='icon-sun'></span>)  }
       </button>
  
    
